b6724ef3a189f47a0984e20419f435e93d8b32f3
Jordan Fontaine <jordanmfontaine@gmail.com>
2022-12-11 17:37:16 -0500

    Added download and save button. Added reset functionality for the ui

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 // SELECTORS
 const form = document.getElementById('generate-form');
-const qrCodeOutput = document.getElementById('qrcode')
+const qr = document.getElementById('qrcode')
 
 
 // FUNCTIONS
 const onSubmit = (e) => {
     e.preventDefault();
+
+    clearUi();
     
     // Since this is an input, we need to select the value. Not just the element
     const url = document.getElementById('url').value;
@@ -20,9 +22,14 @@ const onSubmit = (e) => {
         setTimeout(() => {
             hideSpinner()
             generateQRCode(url, size)
+            
+            setTimeout(() => {
+                const saveUrl = qr.querySelector('img').src;
+                createSaveBtn(saveUrl);
+            }, 50)
         }, 3000)
     }
-}
+} 
 
 const generateQRCode = (url, size) => {
     const qrcode = new QRCode('qrcode', {
@@ -39,6 +46,25 @@ const showSpinner = () => {
 const hideSpinner = () => {
     document.getElementById('spinner').style.display = 'none';
 }
+
+const clearUi = () => {
+    qr.innerHTML = '';
+    const saveLink = document.getElementById('save-link')
+    if (saveLink) {
+        saveLink.remove()
+    }
+}
+
+const createSaveBtn = (saveUrl) => {
+    const link = document.createElement('a');
+    link.id = 'save-link';
+    link.classList = 'bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5';
+    link.href = saveUrl;
+    link.download = 'qrcode'
+    link.innerHTML = 'Download and Save QR Code';
+    document.getElementById('generated').appendChild(link);
+}
+
 hideSpinner()
 
 
